1abf939642415f76fde11f01070f9a616280db4d
Philip <philip.4242@gmail.com>
2017-07-21 15:28:04 -0700

    Refactoring and comment cleanup
    token and browser agent were moved to the top so they can be easily
    changed.
    
    Multiple comments and TODOs were cleaned up and clarified regarding how
    BeautifulSoup and the consolegameswiki site functions.

--- AlthykBot.py
+++ AlthykBot.py
@@ -11,6 +11,8 @@ bot_prefix = "?"
 client = commands.Bot(command_prefix=bot_prefix)
 baseSearchContext = 'https://ffxiv.consolegameswiki.com/mediawiki/index.php?title=Special%3ASearch&profile=default&fulltext=Search&search='
 baseResultContext = 'https://ffxiv.consolegameswiki.com/wiki/'
+userAgent = ''
+clientToken = ''
 
 @client.event
 async def on_ready():
@@ -31,13 +33,9 @@ async def wiki(ctx):
     context provided. It just uses the search function and returns
     the first page title match.'''
 
+    #TODO: return duckduckgo search results instead of failure message
     # Set the default message to be returned
-    #TODO: Replace this with alternative suggestions
-    #TODO: account for spelling mistakes?
-    returnMessage = "Sorry, but I couldn't find that."
-    # Set the User-Agent for later
-    #TODO: Check license. Is this allowed?
-    userAgent = ""
+    returnMessage = "Sorry, but I couldn't find that exact term."
 
     # grab the whole message that triggered the bot
     terms = ctx.message.content
@@ -55,11 +53,15 @@ async def wiki(ctx):
     # now that we have the data, parse it into a BeautifulSoup object
     #TODO: Modify this to only read until the important part of the file.
     soup = BeautifulSoup(data, 'html.parser')
-    # use a feature of BeautifulSoup to find the mw-search-exists class
-    #TODO: Is there a better way?
+    # When a search term has its own page, ffxivwiki tags
+    # it with a specific class (.mw-search-exists). Use a feature of 
+    # BeautifulSoup to find the mw-search-exists class
+    #TODO: Is there a better way? There is BeautifulSoup documentation for
+    # grabbing only part of the page into the object. If there is a hit,
+    # the target class will be close to the top of the page.
     searchResult = soup.select(".mw-search-exists")
     # if the result exists, grab the substring that represents the page and format it into a link
-    # this is pretty ham-fisty, it seems like there is probably a better way to do this with Beautiful Soup.
+    # this is pretty ham-fisted, it seems like there is probably a better way to do this with Beautiful Soup.
     if searchResult != []:
         resultURL = soup.p.a.contents[0]
         resultURL = resultURL.replace("'", "%27")
@@ -69,5 +71,5 @@ async def wiki(ctx):
 
     await client.say(returnMessage)
 
-# Your own token goes here
-client.run("")
+# The token goes here
+client.run(clientToken)
